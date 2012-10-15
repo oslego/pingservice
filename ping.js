5cda8066a8b89588d69c9091dc66dd5a63d8ae5c
Razvan Caliman <razvan.caliman@gmail.com>
2012-10-15 14:50:49 +0100

    initial commit of ping service

---
+++ ping.js
@@ -0,0 +1,50 @@
+var cronJob = require('cron').CronJob,
+    environment = process.env.NODE_ENV || 'development',
+    request = require('request'),
+    config = {}
+    queue = [];
+    
+switch(environment){
+    case 'development':
+        // Runs every 2 seconds, everyday
+        config.cronTime = '*/2 * * * * *'
+        
+        // Localhost test ping
+        queue.push({
+            url: "http://localhost:5000/ping",
+            method: "GET"
+        })
+    break
+
+    case 'production':
+        // Runs once every 30 minutes, everyday
+        config.cronTime = '0 */30 * * * *'
+        
+        // REX App API
+        queue.push({
+            url: "http://api.rexapp.ro/ping",
+            method: "GET",
+            timeout: 30000
+        })
+    break
+}
+
+function handler(error, response, body){
+    if (!error && response.statusCode == 200){
+        console.log(response.statusCode, error)
+    }
+    else{
+        console.log(response.statusCode, "ping", body)
+    }
+}
+
+new cronJob({
+    cronTime: config.cronTime,
+    start: true,
+    onTick: function(){
+        console.log("tick")
+        queue.forEach(function(options){
+            request(options, handler)
+        })
+    }
+})
